ed6834883cbf0289d6c593edb694cabd42d36421
Naoki Ueda <nao@locapoint.com>
2020-09-04 12:26:50 +0900

    Update Axis.cpp

--- Axis.cpp
+++ Axis.cpp
@@ -1,3 +1,14 @@
+/*
+This file is a part of AZ-Nunchuk project.
+https://az-nunchuk.fun/
+
+Author: Naoki Ueda, 2020/09/03
+
+This project use external library, "Arduino Nunchuk".
+
+CC BY 4.0
+This work is licensed under the Creative Commons Attribution 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/.
+*/
 #include <arduino.h>
 #include "pt.h"
 #include "Axis.h"
